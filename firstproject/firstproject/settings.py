fe401cf5c4169dd7b1ad5b468aa31ea6496dec61
seochan99 <gmlcks0513@naver.com>
2021-04-18 23:29:56 +0900

    end

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'firstapp.apps.FirstappConfig',
+    'wordCount.apps.WordcountConfig',
 ]
 
 MIDDLEWARE = [
